f7d5721e5a13fd72594dbc964d117129cf557b8b
koseokkyu <dighrh@naver.com>
2017-04-22 17:22:23 +0900

    constructor

--- poker/src/main/java/com/edu/cnu/poker/Game.java
+++ poker/src/main/java/com/edu/cnu/poker/Game.java
@@ -4,4 +4,7 @@ package com.edu.cnu.poker;
  * Created by user on 2017-04-17.
  */
 public class Game {
+    Deck deck = new Deck(1);
+    Player player1 = new Player(new Hand(deck), PokerType.FIVE);
+    Player player2 = new Player(new Hand(deck), PokerType.FIVE);
 }
--- poker/src/main/java/com/edu/cnu/poker/Player.java
+++ poker/src/main/java/com/edu/cnu/poker/Player.java
@@ -3,5 +3,11 @@ package com.edu.cnu.poker;
 /**
  * Created by user on 2017-04-17.
  */
+
 public class Player {
+    private Hand hand;
+
+    public Player(Hand hand){
+        this.hand = hand;
+    }
 }
