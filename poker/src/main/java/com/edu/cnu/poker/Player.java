ebaeec3e4c04c00b034ec03cadef4b8407f5577b
JungTaepyo <jtp9688@naver.com>
2017-04-22 18:19:50 +0900

    make PokerApplication

--- poker/src/main/java/com/edu/cnu/poker/Player.java
+++ poker/src/main/java/com/edu/cnu/poker/Player.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
  * Created by user on 2017-04-17.
  */
 public class Player {
-
     ArrayList<Card> list = new ArrayList<Card>();
+
     public void getCard(Card card){
         list.add(card);
     }
--- poker/src/main/java/com/edu/cnu/poker/PokerApplication.java
+++ poker/src/main/java/com/edu/cnu/poker/PokerApplication.java
@@ -13,5 +13,17 @@ public class PokerApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello Poker");
+        int i;
+
+        Deck deck = new Deck(1);;
+        Player p1 = new Player();
+        Player p2 = new Player();
+        for(i = 0; i < 5; i++){
+            p1.getCard(deck.drawCard());
+            p2.getCard(deck.drawCard());
+        }
+        p1.showCards();
+        p2.showCards();
+
     }
 }
