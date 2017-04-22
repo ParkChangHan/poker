d678b841e479cb42c3b63e8afc15be85d348a1c9
ParkChangHan <qkck@naver.com>
2017-04-22 16:47:15 +0900

    Player Constructor

--- poker/src/main/java/com/edu/cnu/poker/Game.java
+++ poker/src/main/java/com/edu/cnu/poker/Game.java
@@ -4,4 +4,7 @@ package com.edu.cnu.poker;
  * Created by user on 2017-04-17.
  */
 public class Game {
+    Deck deck = new Deck(1);
+    Player p1 = new Player(deck);
+    Player p2 = new Player(deck);
 }
--- poker/src/main/java/com/edu/cnu/poker/Player.java
+++ poker/src/main/java/com/edu/cnu/poker/Player.java
@@ -4,4 +4,9 @@ package com.edu.cnu.poker;
  * Created by user on 2017-04-17.
  */
 public class Player {
+    Hand hand;
+    public Player(Deck deck){
+        hand = new Hand(deck,PokerType.FIVE);
+    }
+
 }
