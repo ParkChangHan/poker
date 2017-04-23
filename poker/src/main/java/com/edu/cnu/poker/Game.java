aaf15843c37136dd80767d8cf3f5bcd8e2c6d305
koseokkyu <dighrh@naver.com>
2017-04-23 19:18:49 +0900

    Real Constructor

--- poker/src/main/java/com/edu/cnu/poker/Game.java
+++ poker/src/main/java/com/edu/cnu/poker/Game.java
@@ -1,7 +1,32 @@
 package com.edu.cnu.poker;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by user on 2017-04-17.
  */
 public class Game {
+    Deck deck;
+    Player player1;
+    Player player2;
+
+    public Game(Deck deck, Player player1, Player player2){
+        this.deck = deck;
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public void start(){
+        Evaluator evaluator = new Evaluator();
+        List<Card> cardList = Arrays.asList();
+        for(int i=0; i<5; i++){
+            cardList.add(new Card())
+        }
+        String result = evaluator.evaluate(cardList);
+        assertThat(result,is("FLUSH"));
+    }
 }
--- poker/src/main/java/com/edu/cnu/poker/Player.java
+++ poker/src/main/java/com/edu/cnu/poker/Player.java
@@ -1,7 +1,21 @@
 package com.edu.cnu.poker;
 
+import lombok.Data;
+
 /**
  * Created by user on 2017-04-17.
  */
 public class Player {
+    Hand hand;
+
+    public Player(Hand hand){
+        this.hand = hand;
+    }
+
+    public void setHand(Hand hand){
+        this.hand = hand;
+    }
+    public Hand getHand(){
+        return hand;
+    }
 }
